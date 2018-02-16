58eb90681ebecddb9cffc1cd03aefa6c95a22b8c
Irena Shashchuk <ishashchuk@wayfair.com>
2018-02-16 10:15:20 -0500

    Renamed the datasource. made it focused around a single api resource

--- google/data_source_google_project.go
+++ google/data_source_google_project.go
@@ -7,11 +7,11 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
-func dataSourceGoogleComputeProject() *schema.Resource {
+func dataSourceGoogleProject() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceGoogleComputeProjectRead,
+		Read: dataSourceGoogleProjectRead,
 		Schema: map[string]*schema.Schema{
-			"default_service_account": {
+			"name": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -19,25 +19,16 @@ func dataSourceGoogleComputeProject() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"self_link": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
 		},
 	}
 }
 
-func dataSourceGoogleComputeProjectRead(d *schema.ResourceData, meta interface{}) error {
+func dataSourceGoogleProjectRead(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
 
 	project, err := getProject(d, config)
 	if err != nil {
 		return err
-	}
-
-	projectCompResource, err := config.clientCompute.Projects.Get(project).Do()
-	if err != nil {
-		return fmt.Errorf("Error reading project resource: %s", err)
 	}
 
 	projectResManager, err := config.clientResourceManager.Projects.Get(project).Do()
@@ -47,8 +38,7 @@ func dataSourceGoogleComputeProjectRead(d *schema.ResourceData, meta interface{}
 
 	d.SetId(projectResManager.ProjectId)
 	d.Set("project_number", strconv.FormatInt(projectResManager.ProjectNumber, 10))
-	d.Set("default_service_account", projectCompResource.DefaultServiceAccount)
-	d.Set("self_link", projectCompResource.SelfLink)
+	d.Set("name", projectResManager.Name)
 
 	return nil
 }
